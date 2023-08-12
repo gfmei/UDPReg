a60ac683c8a998085b3e0ffdb413e0f20f058992
gfmei <meiguofeng@sina.cn>
2023-08-12 21:44:55 +0200

    Update README.md

--- models/factory.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 3/17/2023 3:47 PM
-# @Author  : Guofeng Mei
-# @Email   : Guofeng.Mei@student.uts.edu.au
-# @File    : factory.py
-# @Software: PyCharm
-

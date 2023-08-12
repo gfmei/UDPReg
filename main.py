28418b03f0bc0f5a9f7e2fdf31b70aa6c7b60fb4
gfmei <meiguofeng@sina.cn>
2023-08-12 21:46:46 +0200

    Update README.md

--- main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 3/17/2023 2:36 PM
-# @Author  : Guofeng Mei
-# @Email   : Guofeng.Mei@student.uts.edu.au
-# @File    : main.py
-# @Software: PyCharm
-
